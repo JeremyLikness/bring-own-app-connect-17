ddfb0cf04b1d3a147bd4137081db43209cb64111
Jeremy Likness <jeremy@jeremylikness.com>
2017-11-09 15:59:42 -0800

    Initial commit of baseline project

---
+++ Src-TodoApi/Models/TodoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+
+        }
+       
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
---
+++ Src-TodoApi/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using TodoApi.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+
+        static Startup()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            string connection = Configuration.GetConnectionString("TodoContext");
+            services.AddDbContext<TodoContext>(opt => opt.UseSqlServer(connection));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder()
+              .ConfigureAppConfiguration((ctx, cfg) =>
+              {
+                  cfg.SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", true) // require the json file!
+          .AddEnvironmentVariables();
+              })
+              .ConfigureLogging((ctx, logging) => { }) // No logging
+              .UseStartup<Startup>()
+              .UseSetting("DesignTime", "true")
+              .Build();
+        }
+    }
+}
